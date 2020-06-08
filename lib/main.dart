b24ae1734ef2be89e7f13efb1d141b96b97155d5
Nick <wachiranduati@gmail.com>
2020-06-08 22:45:09 +0300

    Initialize App

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(HomeApp());
+}
+
+class HomeApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
