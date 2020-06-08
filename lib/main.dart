a076c12e64bbeecabae80d954e0cb289811a4e78
Nick <wachiranduati@gmail.com>
2020-06-08 22:56:49 +0300

    minimal app frame

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,15 @@ void main() {
 class HomeApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(// always begin with a material app
+      home: Scaffold( // second should always be the scaffold
+        appBar: AppBar(
+          title: Text('Flutter App'),//note the use of single quotation marks
+        ),
+        body: Container(// container added to the app...it takes up all the available space in an app
+          color: Colors.teal,
+        ),
+      ),
+    );
   }
 }
