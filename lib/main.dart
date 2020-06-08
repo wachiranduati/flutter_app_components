16714e6aa0228eae1d1ef5c9ee48ff11941226ae
Nick <wachiranduati@gmail.com>
2020-06-08 23:17:08 +0300

    add a row and containers as children. add colours/shades

--- lib/main.dart
+++ lib/main.dart
@@ -7,12 +7,28 @@ void main() {
 class HomeApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(// always begin with a material app
-      home: Scaffold(// scaffold is really important ...else you get some conspicuous red yellow squiglly lines
+    return MaterialApp(
+      home: Scaffold(
         backgroundColor: Colors.white,
-        body: SafeArea( //introduce a safe area wrapped around the container to remove content from above the appbar
-          child: Container(
-            child: Text('Flutter app'), // a text widget to show text on the app...note that the text is placed under the status bar
+        body: SafeArea(
+          child: Row( // note that a container can only hold a single item...a single child
+            children: <Widget>[// comes the Row widget...takes a multitude of children widgets
+              Container(
+                height: 100,
+                width: 100,
+                color: Colors.teal,//note the colour used is from the material theme
+              ),
+              Container(
+                height: 100,
+                width: 100,
+                color: Colors.red.shade300,// to the material colors you can also add a shade to them
+              ),
+              Container(
+                height: 100,
+                width: 100,
+                color: Colors.yellow[200],// another way to add shades to the material colors
+              )
+            ],
           ),
         ),
       )
