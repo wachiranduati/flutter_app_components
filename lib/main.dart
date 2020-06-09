ac3664f349c23e8368171d8d09a8352fc2acad3e
Nick <wachiranduati@gmail.com>
2020-06-09 21:49:36 +0300

    add icon widget

--- lib/main.dart
+++ lib/main.dart
@@ -24,21 +24,9 @@ class HomeApp extends StatelessWidget {
                 width: 100,
                 color: Colors.red.shade300,
               ),
-                //Navigate to the pubsec.yaml file...its been commented out..go over the asset and images import section
-              Image(image: AssetImage('images/speech.png'),
-              height: 100,
-              width: 100,),
-              //Navigate to the pubsec.yaml file...its been commented out..go over the asset and images import section
-
-              Image(image: NetworkImage('https://www.nsinsurance.com/wp-content/uploads/sites/9/2019/08/hippo-3647749_1280.jpg'),
-              height: 100,// you can also load an image directly from the internet
-              width: 100,),
-              Container(
-                height: 100,
-                width: 100,
-                child: Image.asset('images/girl.jpg', height: 100, width: 100,), // yet another way to load an asset image..others include Image.network..Image.file
-                color: Colors.black,// currently its nested as a single child in the container widget
-              ),
+               Icon(Icons.style,// you have access to material icons too via the Icon widget
+                 color: Colors.blue.shade300,
+               size: 20,),
 
 
             ],
