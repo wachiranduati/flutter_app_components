a8b991c980a4dac96fac7ed2600b76bce2bca51e
Nick <wachiranduati@gmail.com>
2020-06-09 06:10:02 +0300

    gesture detection added

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutterappcomponents/products.dart'; // note the import statements for the new product class
 
 void main() {
   runApp(HomeApp());
 }
 
 class HomeApp extends StatelessWidget {
+  // most of the code initializations is done here
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -22,11 +24,22 @@ class HomeApp extends StatelessWidget {
                 width: 100,
                 color: Colors.red.shade300,
               ),
-              Container(
-                height: 100,
-                width: 100,
-                color: Colors.yellow[200],
-              )],
+              GestureDetector( // adding gestures to a widget
+                onTap: (){ // ontap is just one of the many gestures
+                  print('thing tapped');
+                },
+                onDoubleTap: (){ // note that an anonymous function is passed to the gesture
+                  print('double tapped'); // you can also print out statements..to view them open the run tab
+                  // also remember to add ; semicolon to the end of every line of code
+                },
+                child: Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.yellow[500],
+                ),
+              ),
+
+            ],
           ),
         ),
       )
